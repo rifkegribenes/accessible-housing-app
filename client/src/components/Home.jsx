84a0fb4dd30287dba50793c0fb21584ce38e44f0
rifkegribenes <rifkegribenes@gmail.com>
2019-09-19 22:46:38 -0700

    home component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Home = props => (
-  <div className={props.classes.home} data-test="component-home">
-    Home
-  </div>
-);
+const Home = props => {
+  const { classes } = props;
+  return (
+    <div className={classes.home} data-test="component-home">
+      <h2 className={classes.heroHead}>Accessible Housing in Portland</h2>
+      <form className={classes.heroForm}>
+        <input
+          className={classes.heroInput}
+          type="text"
+          placeholder="Zip code"
+          name="zip"
+          id="zip"
+        />
+      </form>
+    </div>
+  );
+};
 
 Home.propTypes = {
   classes: PropTypes.object
