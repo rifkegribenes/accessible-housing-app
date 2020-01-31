e935272c8bb348f09cb58df5515b0de177a4420f
rifkegribenes <rifkegribenes@gmail.com>
2020-01-30 22:21:45 -0800

    try redirect hash?

--- app/routes/staticRoutes.js
+++ app/routes/staticRoutes.js
@@ -14,7 +14,7 @@ const staticCtrl = require("../static.ctrl");
 
 // Catch client-side routes that don't exist on the back-end.
 // Redirects to /#/redirect={route}/{optional_id}
-// router.get('/:client_route/:id?', staticCtrl.redirectHash);
+router.get("/:client_route/:id?", staticCtrl.redirectHash);
 
 // Serve client frontend.
 router.get("/", staticCtrl.serveClient);
